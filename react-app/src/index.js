1a9fb47058dd3703b930e8348736645ba0724cdc
tanbre <tannor@breitigam.email>
2021-03-29 15:54:19 -0700

    added redux starting with authentication

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from "react-redux"
+import { BrowserRouter } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
+import configureStore from "./store"
+
+const store = configureStore()
+
+const Root = () => {
+  return (
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
+)
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Root />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
