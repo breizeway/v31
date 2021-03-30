18c434d9664afff411faa2d8cbbd856f33614566
tanbre <tannor@breitigam.email>
2021-03-30 15:28:41 -0700

    added list_follows table

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,3 +2,4 @@ from .db import db
 from .user import User
 from .list import List
 from .pick import Pick
+from .list_follows import list_follows
--- app/models/list_follows.py
+++ app/models/list_follows.py
@@ -1,10 +1,9 @@
+from sqlalchemy.schema import ForeignKey
+
 from .db import db
-# from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.schema import Column, ForeignKey, Table
 
-# Base = declarative_base()
 
-follows = db.Table(
-    "follows",
+list_follows = db.Table(
+    "list_follows",
     db.Column("user_id", db.Integer, ForeignKey("users.id"), primary_key=True),
     db.Column("list_id", db.Integer, ForeignKey("lists.id"), primary_key=True))
