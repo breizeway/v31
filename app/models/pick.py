abc0eac3abf44c1affb186115fbd784372fe9cf6
tanbre <tannor@breitigam.email>
2021-04-02 08:11:48 -0700

    added original_poster to picks model

--- app/models/pick.py
+++ app/models/pick.py
@@ -11,6 +11,7 @@ class Pick(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     overview = db.Column(db.Text)
+    original_poster = db.Column(db.String(500))
     date = db.Column(db.Date, nullable=False)
     media_id = db.Column(db.Integer, nullable=False)
     imdb_id = db.Column(db.String(50))
@@ -23,6 +24,7 @@ class Pick(db.Model):
                 'title': self.title,
                 'overview': self.overview,
                 'date': self.date,
+                'original_poster': self.original_poster,
                 'media_id': self.media_id,
                 'imdb_id': self.imdb_id,
                 'list_id': self.list_id}
@@ -32,6 +34,7 @@ class Pick(db.Model):
                 'title': self.title,
                 'overview': self.overview,
                 'date': self.date,
+                'original_poster': self.original_poster,
                 'media_id': self.media_id,
                 'imdb_id': self.imdb_id,
                 'list_id': self.list_id,
